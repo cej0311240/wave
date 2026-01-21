4f4e57990c1ada8d274f4155a1472883b3b41f31
최은지 <cej0311240@gmail.com>
2026-01-21 12:11:35 +0900

    Manual deploy React app

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./styles/reset.css";
+import "./styles/fonts.css";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import Wave from "./pages/Wave";
+import About from "./pages/About";
+import Summary from "./pages/Summary";
+import Info from "./pages/Info";
+
+const App = () => {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Wave />} />
+          <Route path="info" element={<Info />} />
+          <Route path="about" element={<About />}>
+            <Route index element={<Summary title="사업개요"/>} />
+            <Route path="consumer" element={<Summary title="컨슈머커머스"/>} />
+            <Route path="seller" element={<Summary title="셀러커머스"/>} />
+            <Route path="crossborder" element={<Summary title="크로스보더커머스"/>} />
+          </Route>
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
+}
+
+export default App;
